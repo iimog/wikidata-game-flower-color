b763b7da949f58f37a7be600672c5d47e7af03ba
Markus Ankenbrand <markus@ankenbrand.me>
2017-01-18 15:45:41 +0100

    Add generated getters/setters

--- src/AppBundle/Entity/Tile.php
+++ src/AppBundle/Entity/Tile.php
@@ -25,4 +25,62 @@ class Tile
      * @ORM\Column(type="boolean")
      */
     private $finished;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set wikidataId
+     *
+     * @param string $wikidataId
+     *
+     * @return Tile
+     */
+    public function setWikidataId($wikidataId)
+    {
+        $this->wikidata_id = $wikidataId;
+
+        return $this;
+    }
+
+    /**
+     * Get wikidataId
+     *
+     * @return string
+     */
+    public function getWikidataId()
+    {
+        return $this->wikidata_id;
+    }
+
+    /**
+     * Set finished
+     *
+     * @param boolean $finished
+     *
+     * @return Tile
+     */
+    public function setFinished($finished)
+    {
+        $this->finished = $finished;
+
+        return $this;
+    }
+
+    /**
+     * Get finished
+     *
+     * @return boolean
+     */
+    public function getFinished()
+    {
+        return $this->finished;
+    }
 }
