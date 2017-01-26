ac8ecf4693fda18eaedc8f4fa777673b4a70deac
Markus Ankenbrand <markus@ankenbrand.me>
2017-01-26 11:36:23 +0100

    Change route for homepage

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,9 +18,7 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('default/base.html.twig');
     }
 
     /**
